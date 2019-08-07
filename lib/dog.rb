1594132779e564fb92374487eae3196a853d0e17
Renee Sims <info@reneesoul.com>
2019-08-07 07:07:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
   def initialize(dogs_name, breed)
-    @name = name
+    @name = dogs_name
     @breed = breed
   end
   
-  def breed=(mutt)
-    @breed = mutt
-  end
+  # def breed=(mutt)
+  #   @breed = mutt
+  # end
   
-  def breed(mutt)
-    @breed = mutt.capitalize
-  end
+  # def breed(mutt)
+  #   @breed = mutt.capitalize
+  # end
   
 end
